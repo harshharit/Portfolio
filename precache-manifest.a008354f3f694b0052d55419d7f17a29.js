d3490955c56e89ef8915aed47f83ac04ced4f6be
harshharit <harshharit20@gmail.com>
2022-06-30 03:23:40 +0530

    Updates

---
+++ precache-manifest.a008354f3f694b0052d55419d7f17a29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0f9c738028c97a9c777f4bd5c0adb7",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "96ed8075e498c238caf5",
+    "url": "/Portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "3f0b08ae87aa33b6bdc2",
+    "url": "/Portfolio/static/css/main.57ca69ec.chunk.css"
+  },
+  {
+    "revision": "96ed8075e498c238caf5",
+    "url": "/Portfolio/static/js/2.b8eebcff.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.b8eebcff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0b08ae87aa33b6bdc2",
+    "url": "/Portfolio/static/js/main.8484805c.chunk.js"
+  },
+  {
+    "revision": "c125469937b460e6de34",
+    "url": "/Portfolio/static/js/runtime-main.3379861c.js"
+  }
+]);
